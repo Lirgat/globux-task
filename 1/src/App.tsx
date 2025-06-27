ce852b8b1965b99c05f3210bdc68fe542a53739d
vladimir v <veb455@mail.ru>
2025-06-27 22:22:15 +0300

    refactor: use prettier

--- 1/src/App.tsx
+++ 1/src/App.tsx
@@ -1,27 +1,45 @@
-import { useState } from 'react';
-import Box from '@mui/material/Box';
-import SearchBar from './components/SearchBar/SearchBar';
-import UserCard from './components/UserCard/UserCard';
-import UserModal from './components/UserModal/UserModal';
-import useUsers from './hooks/useUsers';
-import type { User } from './types/type';
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import SearchBar from "./components/SearchBar/SearchBar";
+import UserCard from "./components/UserCard/UserCard";
+import UserModal from "./components/UserModal/UserModal";
+import useUsers from "./hooks/useUsers";
+import type { User } from "./types/type";
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const [selectedUser, setSelectedUser] = useState<null | User>(null);
   const users = useUsers(searchTerm);
 
   return (
-    <Box sx={{ maxWidth: 1200, margin: 'auto', padding: 3 }}>
-      <SearchBar searchTerm={searchTerm} onSearch={(term) => setSearchTerm(term)} />
+    <Box sx={{ margin: "auto", padding: "64px 80px" }}>
+      <SearchBar
+        searchTerm={searchTerm}
+        onSearch={(term) => setSearchTerm(term)}
+      />
 
-      <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', gap: .5, alignItems: 'center', marginTop: 2 }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+          gap: "25px",
+          alignItems: "center",
+          marginTop: "32px",
+        }}
+      >
         {users.map((user) => (
-          <UserCard key={user.id} user={user} onClick={() => setSelectedUser(user)} />
+          <UserCard
+            key={user.id}
+            user={user}
+            onClick={() => setSelectedUser(user)}
+          />
         ))}
       </Box>
 
-      {selectedUser && <UserModal user={selectedUser} onClose={() => setSelectedUser(null)} />}
+      {selectedUser && (
+        <UserModal user={selectedUser} onClose={() => setSelectedUser(null)} />
+      )}
     </Box>
   );
 };
